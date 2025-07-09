3824346474189e16164f13de054bdaee03b13309
canedoa <c.canedo.amaya96@gmail.com>
2025-07-08 22:31:35 -0600

    Modificación del sidebar para que colapse

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -1,16 +1,64 @@
-import { Component } from '@angular/core';
+import { Component, Input, signal, computed } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { MatListModule } from '@angular/material/list';
 import { MatIconModule } from '@angular/material/icon';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
+import { Output, EventEmitter } from '@angular/core';
 
+export type MenuItem = {
+  icon: string;
+  label: string;
+  route?: string;
+};
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [MatListModule, MatIconModule, MatCardModule, MatIconModule,MatButtonModule],
+  imports: [
+    MatListModule,
+    MatIconModule,
+    MatCardModule,
+    MatIconModule,
+    MatButtonModule,
+    CommonModule,
+  ],
   templateUrl: './sidebar.component.html',
-  styleUrls: ['./sidebar.component.scss']
+  styleUrls: ['./sidebar.component.scss'],
 })
-export class SidebarComponent {}
+export class SidebarComponent {
+  @Output() itemSelected = new EventEmitter<void>();
 
+  onItemClick() {
+    this.itemSelected.emit();
+  }
+
+  sideNavCollapsed = signal(false);
+
+  @Input() set collapsed(val: boolean) {
+    this.sideNavCollapsed.set(val);
+  }
+
+  menuItems = signal<MenuItem[]>([
+    {
+      icon: 'folder',
+      label: 'Mis Archivos',
+      route: 'dashboard',
+    },
+    {
+      icon: 'people',
+      label: 'Compartidos',
+      route: 'dashboard',
+    },
+    {
+      icon: 'delete',
+      label: 'Papeleria',
+      route: 'dashboard',
+    },
+  ]);
+
+  profilePicSize = computed(() => ({
+    width: this.sideNavCollapsed() ? '55px' : '141px',
+    height: this.sideNavCollapsed() ? '50px' : '101px',
+  }));
+}
--- src/app/layout/dashboard-layout/dashboard-layout.component.ts
+++ src/app/layout/dashboard-layout/dashboard-layout.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, signal, computed } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { AuthService } from '../../services/auth.service';
@@ -6,22 +6,32 @@ import { Router } from '@angular/router';
 import { MatIconModule } from '@angular/material/icon';
 import { MatIconButton } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
-import { SidebarComponent } from "../../components/sidebar/sidebar.component";
-
+import { SidebarComponent } from '../../components/sidebar/sidebar.component';
 
 @Component({
   selector: 'app-dashboard-layout',
   standalone: true,
-  imports: [CommonModule, MatToolbarModule, MatIconButton, MatIconModule, MatSidenavModule, SidebarComponent],
+  imports: [
+    CommonModule,
+    MatToolbarModule,
+    MatIconButton,
+    MatIconModule,
+    MatSidenavModule,
+    SidebarComponent,
+  ],
   templateUrl: './dashboard-layout.component.html',
   styleUrls: ['./dashboard-layout.component.scss'],
 })
 export class DashboardLayoutComponent {
   user = { name: 'Celeste' };
+  collapsed = signal(false);
+
+  sidenavWidth = computed(() => (this.collapsed() ? '65px' : '250px'));
 
   constructor(private authService: AuthService, private router: Router) {}
+
   logout(): void {
     this.authService.logout();
-    this.router.navigate(['./login']);
+    this.router.navigate(['/login']);
   }
 }
