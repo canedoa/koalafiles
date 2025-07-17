2ebf4e6f85ac4f6faadc772bd9e3f01a24bdca58
canedoa <c.canedo.amaya96@gmail.com>
2025-07-16 19:38:23 -0600

    modificacion enviroments produccion

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiBackendUrl: 'https://koalafiles-backend.up.railway.app',
+  apiBackendUrl: 'koalafiles-backend-production.up.railway.app',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiBackendUrl: 'https://koalafiles-backend.up.railway.app',
+  apiBackendUrl: 'http://localhost:3000',
 };
